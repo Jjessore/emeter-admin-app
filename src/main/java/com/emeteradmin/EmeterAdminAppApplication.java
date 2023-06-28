f43c53d876b1e4f829a595ff6ba0da896a80b9f4
Jessore <tech.jessore@gmail.com>
2023-06-28 14:48:59 +0530

    Master commit

---
+++ src/main/java/com/emeteradmin/EmeterAdminAppApplication.java
@@ -0,0 +1,13 @@
+package com.emeteradmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmeterAdminAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmeterAdminAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/emeteradmin/EmeterAdminAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emeteradmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmeterAdminAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
